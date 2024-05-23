70512150b2cf0aa9c763fee6477f9a9101cce76f
salimsulaiman <salimsulaiman75@gmail.com>
2024-05-23 12:00:54 +0700

    fix bug background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 import Bio from "./Bio";
 import SideBar from "./SideBar";
 import Background from "../assets/background.png";
 import BackgroundDark from "../assets/backgroundDark.png";
+import { ThemeContext } from "../ThemeContext";
 
 function Layout() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="w-full min-h-screen bg-slate-100 pt-4 pb-12 font-poppins bg-background dark:bg-backgroundDark bg-cover bg-fixed">
+    <div
+      className="w-full min-h-screen bg-slate-100 pt-4 pb-12 font-poppins bg-cover bg-fixed"
+      style={{ backgroundImage: `url(${theme == "dark" ? BackgroundDark : Background})` }}
+    >
       <div className="max-w-[1124px] mx-auto pt-4 px-4 sm:px-8 flex justify-end">
         <Navbar />
       </div>
